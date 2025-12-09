a36104f3ae2b5a356de8a4031d3e18b95a9cdaad
Mirsait <mavcharenko@gmail.com>
2025-12-09 20:39:22 +0300

    Initial Day 09

---
+++ day09/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/Mirsait/advent-of-code-2025/common"
+)
+
+func main() {
+	common.Clear()
+	fmt.Println(common.Hello())
+
+	// lines, err := common.ReadFileByLines("input.txt")
+
+	// if err != nil {
+	// 	fmt.Println(err.Error())
+	// 	return
+	// }
+}
